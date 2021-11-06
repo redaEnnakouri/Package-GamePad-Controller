f26cb89e2b6a447c7733d1ca100087dd4eda2fb0
Reda <ennakouri@ennakouri-XPS-13-9350>
2021-11-06 20:01:39 +0100

    install package npm

---
+++ package-game-pad-controller/dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import { defineComponent } from 'vue';
+import PackageGamePadController from '@/package-game-pad-controller.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    PackageGamePadController
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <package-game-pad-controller />
+  </div>
+</template>
